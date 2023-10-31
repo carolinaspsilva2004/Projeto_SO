4e20f05cf045cfcc3186e9701581eb2b06f0a080
TKahk <pedro.salgado@ua.pt>
2023-10-31 00:06:57 +0000

    O space.sh tá a dar print ás subpastas, so que os filtros não estão a funcionar

--- space.sh
+++ space.sh
@@ -1,86 +1,113 @@
-#!/bin/bash
+    # funçao que permite a visualização do espaço
+    # ocupado pelos ficheiros selecionados na(s) diretoria(s) que lhe é(são) passada(s) 
+    # como argumento e em todas as subdiretorias destas.
 
-# Função para exibir a ajuda
-function show_help() {
-  echo "Uso: $0 [opções] diretório"
-  echo "Opções:"
-  echo "  -n PATTERN  Filtrar por nome de arquivo usando uma expressão regular"
-  echo "  -d DATE    Filtrar por data de modificação (exemplo: 'Sep 10 10:00')"
-  echo "  -s SIZE    Filtrar por tamanho mínimo (em bytes)"
-  echo "  -r         Ordenar a saída em ordem reversa"
-  echo "  -a         Ordenar a saída por nome (ordem alfabética)"
-  echo "  -l LIMIT   Limitar o número de linhas na saída"
-  echo "  -h         Mostrar esta ajuda"
-  exit 1
-}
+    #!/bin/bash
 
-# Inicialização de variáveis padrão
-FILTER_NAME=""
-FILTER_DATE=""
-FILTER_SIZE=""
-REVERSE_SORT=""
-ALPHABETICAL_SORT=""
-LIMIT=""
-DIRECTORY=""
+    # Variáveis padrão
+    show_all_files=1
+    regex_filter=""
+    max_modification_date=""
+    min_file_size=""
+    reverse_order=""
+    sort_by_name=""
+    limit_lines=""
+    dir="."
 
-# Processar as opções de linha de comando
-while getopts ":n:d:s:rahl:" opt; do
-  case $opt in
-    n) FILTER_NAME="$OPTARG";;
-    d) FILTER_DATE="$OPTARG";;
-    s) FILTER_SIZE="$OPTARG";;
-    r) REVERSE_SORT="true";;
-    a) ALPHABETICAL_SORT="true";;
-    l) LIMIT="$OPTARG";;
-    h) show_help;;
-    \?) echo "Opção inválida: -$OPTARG" >&2; exit 1;;
-  esac
-done
+    # Função para calcular o espaço ocupado por um arquivo ou diretório
+    function calcular_espaco() {
+      local item="$1"
+      local espaco="NA"
 
-# Obter o diretório como o último argumento não processado
-shift $((OPTIND - 1))
-DIRECTORY="$1"
-
-# Verificar se o diretório existe
-if [ ! -d "$DIRECTORY" ]; then
-  echo "Erro: O diretório especificado não existe."
-  exit 1
-fi
-
-# Função para listar os arquivos que correspondem aos critérios
-function list_files() {
-  local dir="$1"
-  local filter_name="$2"
-  local filter_date="$3"
-  local filter_size="$4"
-
-  find "$dir" -type f | while read -r file; do
-    # Verificar os critérios de filtro
-    if [ -z "$filter_name" ] || [[ "$(basename "$file")" =~ $filter_name ]]; then
-      if [ -z "$filter_date" ] || [[ "$(date -r "$file" +"%b %d %H:%M")" == $filter_date ]]; then
-        if [ -z "$filter_size" ] || [ "$(stat -c %s "$file")" -ge $filter_size ]; then
-          echo "$(stat -c %s "$file") $(realpath --relative-to="$DIRECTORY" "$file") $(date -r "$file" +"%b %d %H:%M")"
-        fi
+      if [ -e "$item" ]; then
+          espaco=$(du -b -sh "$item" 2>/dev/null | cut -f1)
+          if [ -z "$espaco" ]; then
+              espaco="NA"
+          fi
       fi
+
+      echo "$espaco"
+    }
+
+    # Função de ajuda
+    function exibir_ajuda() {
+        echo "Uso: $0 [-n <regex>] [-d <data>] [-s <tamanho>] [-r] [-a] [-l <linhas>] <diretório>"
+        echo "Opções:"
+        echo "  -n <regex>      Filtrar por expressão regular no nome do arquivo"
+        echo "  -d <data>       Filtrar por data máxima de modificação (formato AAAA-MM-DD)"
+        echo "  -s <tamanho>    Filtrar por tamanho mínimo de arquivo (em bytes)"
+        echo "  -r              Ordenar em ordem reversa"
+        echo "  -a              Ordenar por nome de arquivo"
+        echo "  -l <linhas>     Limitar o número de linhas da tabela"
+        echo "  <diretório>     Diretório a ser analisado (padrão: diretório atual)"
+    }
+
+    function processar_diretorio() {
+      local dir="$1"
+      
+      find_cmd="find $dir"
+
+      if [ -n "$regex_filter" ]; then
+          find_cmd="$find_cmd -type f -regex "$regex_filter""
+      fi
+
+      if [ -n "$max_modification_date" ]; then
+          find_cmd="$find_cmd -type f -newermt $max_modification_date"
+      fi
+
+      if [ -n "$min_file_size" ]; then
+          find_cmd="$find_cmd -type f -size +${min_file_size}c"
+      fi
+
+      if [ -n "$regex_filter" ] || [ -n "$max_modification_date" ] || [ -n "$min_file_size" ]; then
+          find_cmd="$find_cmd -printf '%p\n'"
+      else
+          find_cmd="$find_cmd -type d -printf '%p\n'"
+      fi
+
+      if [ "$sort_by_name" = true ]; then
+          sort_cmd="sort"
+      elif [ "$reverse" = true ]; then
+          sort_cmd="sort -rh"
+      else
+          sort_cmd="sort -h"
+      fi
+
+      eval $find_cmd | while IFS= read -r item; do
+          total_size=$(du -sh "$item" | cut -f1)
+          printf "%s: %s\n" "$total_size" "$item"
+      done | $sort_cmd
+
+      if [ -n "$limit_lines" ]; then
+          head -n $limit_lines
+      fi
+    }
+
+
+    # Processar argumentos
+    while getopts "n:d:s:ral:" opt; do
+        case $opt in
+            n) regex_filter="$OPTARG" ;;
+            d) max_modification_date="$OPTARG" ;;
+            s) min_file_size="$OPTARG" ;;
+            r) reverse_order=1 ;;
+            a) sort_by_name=1 ;;
+            l) limit_lines="$OPTARG" ;;
+            \?) exibir_ajuda; exit 1 ;;
+        esac
+    done
+    shift $((OPTIND-1))
+
+
+    # Verificar se o diretório foi passado como argumento
+
+    if [ -n "$1" ]; then
+        dir="$1"
+    else
+        dir="."  # Defina o diretório padrão se nenhum diretório for especificado.
     fi
-  done
-}
 
-# Listar os arquivos que correspondem aos critérios
-files=$(list_files "$DIRECTORY" "$FILTER_NAME" "$FILTER_DATE" "$FILTER_SIZE")
 
-# Ordenar os arquivos com base nas opções de ordenação
-if [ -n "$REVERSE_SORT" ]; then
-  files=$(echo "$files" | sort -nr)
-elif [ -n "$ALPHABETICAL_SORT" ]; then
-  files=$(echo "$files" | sort)
-fi
+    # Gerar comando find baseado em opções
+    processar_diretorio "$dir"
 
-# Limitar o número de linhas na saída
-if [ -n "$LIMIT" ]; then
-  files=$(echo "$files" | head -n "$LIMIT")
-fi
-
-# Exibir a tabela resultante
-echo "SIZE NAME DATE"
-echo "$files"
--- spacecheck.sh
+++ spacecheck.sh
@@ -1,108 +1,99 @@
-# funçao que permite a visualização do espaço
-# ocupado pelos ficheiros selecionados na(s) diretoria(s) que lhe é(são) passada(s) 
-# como argumento e em todas as subdiretorias destas.
+    # funçao que permite a visualização do espaço
+    # ocupado pelos ficheiros selecionados na(s) diretoria(s) que lhe é(são) passada(s) 
+    # como argumento e em todas as subdiretorias destas.
 
-#!/bin/bash
+    #!/bin/bash
 
-# Variáveis padrão
-show_all_files=1
-regex_filter=""
-max_modification_date=""
-min_file_size=""
-reverse_order=""
-sort_by_name=""
-limit_lines=""
-dir="."
+    # Variáveis padrão
+    show_all_files=1
+    regex_filter=""
+    max_modification_date=""
+    min_file_size=""
+    reverse_order=""
+    sort_by_name=""
+    limit_lines=""
+    dir="."
 
-# Função para calcular o espaço ocupado por um arquivo ou diretório
-function calcular_espaco() {
-    local item="$1"
-    local espaco="NA"
+    # Função para calcular o espaço ocupado por um arquivo ou diretório
+    function calcular_espaco() {
+        local item="$1"
+        local espaco="NA"
 
-    if [ -e "$item" ]; then
-        espaco=$(du -sh "$item" 2>/dev/null | cut -f1)
-        if [ -z "$espaco" ]; then
-            espaco="NA"
+        if [ -e "$item" ]; then
+            espaco=$(du -b -sh "$item" 2>/dev/null | cut -f1)
+            if [ -z "$espaco" ]; then
+                espaco="NA"
+            fi
         fi
-    fi
 
-    echo "$espaco"
-}
+        echo "$espaco"
+    }
 
-# Função de ajuda
-function exibir_ajuda() {
-    echo "Uso: $0 [-n <regex>] [-d <data>] [-s <tamanho>] [-r] [-a] [-l <linhas>] <diretório>"
-    echo "Opções:"
-    echo "  -n <regex>      Filtrar por expressão regular no nome do arquivo"
-    echo "  -d <data>       Filtrar por data máxima de modificação (formato AAAA-MM-DD)"
-    echo "  -s <tamanho>    Filtrar por tamanho mínimo de arquivo (em bytes)"
-    echo "  -r              Ordenar em ordem reversa"
-    echo "  -a              Ordenar por nome de arquivo"
-    echo "  -l <linhas>     Limitar o número de linhas da tabela"
-    echo "  <diretório>     Diretório a ser analisado (padrão: diretório atual)"
-}
+    # Função de ajuda
+    function exibir_ajuda() {
+        echo "Uso: $0 [-n <regex>] [-d <data>] [-s <tamanho>] [-r] [-a] [-l <linhas>] <diretório>"
+        echo "Opções:"
+        echo "  -n <regex>      Filtrar por expressão regular no nome do arquivo"
+        echo "  -d <data>       Filtrar por data máxima de modificação (formato AAAA-MM-DD)"
+        echo "  -s <tamanho>    Filtrar por tamanho mínimo de arquivo (em bytes)"
+        echo "  -r              Ordenar em ordem reversa"
+        echo "  -a              Ordenar por nome de arquivo"
+        echo "  -l <linhas>     Limitar o número de linhas da tabela"
+        echo "  <diretório>     Diretório a ser analisado (padrão: diretório atual)"
+    }
 
-# Processar argumentos
-while getopts "n:d:s:ra:l:" opt; do
-    case $opt in
-        n) regex_filter="$OPTARG" ;;
-        d) max_modification_date="$OPTARG" ;;
-        s) min_file_size="$OPTARG" ;;
-        r) reverse_order=1 ;;
-        a) sort_by_name=1 ;;
-        l) limit_lines="$OPTARG" ;;
-        \?) exibir_ajuda; exit 1 ;;
-    esac
-done
-shift $((OPTIND-1))
+    # Processar argumentos
+    while getopts "n:d:s:ral:" opt; do
+        case $opt in
+            n) regex_filter="$OPTARG" ;;
+            d) max_modification_date="$OPTARG" ;;
+            s) min_file_size="$OPTARG" ;;
+            r) reverse_order=1 ;;
+            a) sort_by_name=1 ;;
+            l) limit_lines="$OPTARG" ;;
+            \?) exibir_ajuda; exit 1 ;;
+        esac
+    done
+    shift $((OPTIND-1))
 
 
-# Verificar se o diretório foi passado como argumento
+    # Verificar se o diretório foi passado como argumento
 
-diretório() {
-    local arg="$1"    
-    local ult_arg="${!#}"  # Acesse o último argumento usando !#
-
-    if [ -d "$arg" ]; then
-        dir="$arg"
-    elif [ -d "$ult_arg" ]; then
-        dir="$ult_arg"
+    if [ -n "$1" ]; then
+        dir="$1"
     else
-        echo "Diretório inválido: $arg"
-        exit 1
+        dir="."  # Defina o diretório padrão se nenhum diretório for especificado.
     fi
-}
-
-diretório "$@"
 
 
-# Gerar comando find baseado em opções
-find_cmd="find $dir"
+    # Gerar comando find baseado em opções
+    find_cmd="find $dir"
 
-if [ -n "$regex_filter" ]; then
-    find_cmd="$find_cmd -type f -regex '$regex_filter'"
-else
-    find_cmd="$find_cmd -type f"
-fi
+    if [ -n "$regex_filter" ]; then
+        find_cmd="$find_cmd -type f -regex "$regex_filter""
+    else
+        find_cmd="$find_cmd -type f"
+    fi
 
-if [ -n "$max_modification_date" ]; then
-    find_cmd="$find_cmd -newermt $max_modification_date"
-fi
 
-if [ -n "$min_file_size" ]; then
-    find_cmd="$find_cmd -size +${min_file_size}c"
-fi
+    if [ -n "$max_modification_date" ]; then
+        find_cmd="$find_cmd -newermt $max_modification_date"
+    fi
 
-# Executar o comando find e calcular espaço ocupado
-if [ -n "$limit_lines" ]; then
-    $find_cmd -exec echo {} \; | while read -r item; do
-        espaco=$(calcular_espaco "$item")
-        printf "%s\t%s\n" "$espaco" "$item"
-    done | ($sort_by_name && ($reverse_order && sort -k1,1 -rh || sort -k1,1 -h) || $reverse_order && sort -r -k1,1 -h || sort -k1,1 -h) | head -n "$limit_lines"
-else
-    $find_cmd -exec echo {} \; | while read -r item; do
-        espaco=$(calcular_espaco "$item")
-        printf "%s\t%s\n" "$espaco" "$item"
-    done | ($sort_by_name && ($reverse_order && sort -k1,1 -rh || sort -k1,1 -h) || $reverse_order && sort -r -k1,1 -h || sort -k1,1 -h)
-fi
+    if [ -n "$min_file_size" ]; then
+        find_cmd="$find_cmd -size +${min_file_size}c"
+    fi
+
+    # Executar o comando find e calcular espaço ocupado
+    if [ -n "$limit_lines" ]; then
+        $find_cmd -exec echo {} \; | while read -r item; do
+            espaco=$(calcular_espaco "$item")
+            printf "%s\t%s\n" "$espaco" "$item"
+        done | ($sort_by_name && ($reverse_order && sort -k1,1 -rh || sort -k1,1 -h) || $reverse_order && sort -r -k1,1 -h || sort -k1,1 -h) | head -n "$limit_lines"
+    else
+        $find_cmd -exec echo {} \; | while read -r item; do
+            espaco=$(calcular_espaco "$item")
+            printf "%s\t%s\n" "$espaco" "$item"
+        done | ($sort_by_name && ($reverse_order && sort -k1,1 -rh || sort -k1,1 -h) || $reverse_order && sort -r -k1,1 -h || sort -k1,1 -h)
+    fi
 
